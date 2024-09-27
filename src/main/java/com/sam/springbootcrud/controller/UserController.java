b38757f997add2388d36cfe2ed7da94b3183cab1
Savin Semen <sky.sam0714@gmail.com>
2024-09-27 17:36:25 +0300

    Changed mapping

--- src/main/java/com/sam/springbootcrud/controller/UserController.java
+++ src/main/java/com/sam/springbootcrud/controller/UserController.java
@@ -2,13 +2,15 @@ package com.sam.springbootcrud.controller;
 
 import com.sam.springbootcrud.models.User;
 import com.sam.springbootcrud.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -17,6 +19,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
@@ -27,35 +30,34 @@ public class UserController {
         return "users";
     }
 
-    @GetMapping(value = "/users", params = "updateUserForm")
-    public String updateUserForm(@RequestParam("updateUserForm") Long id, ModelMap model){
+    @GetMapping(value = "/users/update/{id}")
+    public String updateUserAdminForm(@PathVariable("id") Long id, ModelMap model){
         model.addAttribute("user_id",id);
-        System.out.println("user update "+id);
         return "update_user_form";
     }
 
-    @GetMapping(value = "/users", params = "addUserForm")
-    public String addUserForm(){
+    @GetMapping(value = "/users/create")
+    public String addUserAdminForm(){
         return "add_user_form";
     }
 
 
-    @PostMapping(value = "/users", params = "changeUser")
-    public String changeUser(@RequestParam("changeUser") Long id, @ModelAttribute User user, ModelMap model) {
+    @PostMapping(value = "/users/update/{id}")
+    public String changeUser(@PathVariable("id") Long id, @ModelAttribute User user) {
         user.setId(id);
         userService.updateUser(user);
-        return printUsers(model);
+        return "redirect:/users";
     }
 
-    @PostMapping(value = "/users", params = "deleteUser")
-    public String deleteUser(@RequestParam("deleteUser") Long id, ModelMap model){
+    @PostMapping(value = "/users/delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id){
         userService.deleteUserById(id);
-        return printUsers(model);
+        return "redirect:/users";
     }
 
-    @PostMapping(value = "/users", params = "addUser")
-    public String createUser(@ModelAttribute User user, ModelMap model){
+    @PostMapping(value = "/users/create")
+    public String createUser(@ModelAttribute User user){
         userService.createUser(user);
-        return printUsers(model);
+        return "redirect:/users";
     }
 }
